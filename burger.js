fe1e11d8bb9e90c027005b3f8332830b0298e2f6
Polina Novoselova <polka.nov@mail.ru>
2023-04-16 22:58:19 +0200

    add burger-menu

--- burger.js
+++ burger.js
@@ -1,13 +1,15 @@
 const BURGER_OPENED_CLASSNAME = 'burger__open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
 
-const bodyNode = document.querySelector('body');
+const bodyNode_2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerContentNode = document.querySelector('.js-burger__content')
+const burgerContentNode = document.querySelector('.js-burger__content');
+const burgerItem = document.querySelector('.burger__content');
 
 burgerBtnNode.addEventListener('click', toggleBurger);
+burgerItem.addEventListener("click",toggleBurger);
 
 burgerNode.addEventListener('click', (event) => {
     const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
@@ -19,6 +21,6 @@ burgerNode.addEventListener('click', (event) => {
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    bodyNode_2.classList.toggle(BODY_FIXED_CLASSNAME_2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
 }
